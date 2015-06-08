2372274e506502eb28ab5e21b6f4bf6635a55bb6
zhanghuan <m13818239270@163.com>
2015-06-09 01:32:55 +0800

    优惠支付宝回调地址

--- resource/config.js
+++ resource/config.js
@@ -2,7 +2,7 @@ var config = {
     partner:'2088911134104503' //合作身份者id，以2088开头的16位纯数字
     ,key:'ems66ios369p7ef42hr0obm76durfljp'//安全检验码，以数字和字母组成的32位字符
     ,seller_email:'admin@brother-express.com' //卖家支付宝帐户 必填
-    ,host:'http://localhost:3009/'
+    ,host:'http://www.brother-express.com/'
     ,transport:'http' //访问模式,根据自己的服务器是否支持ssl访问，若支持请选择https；若不支持请选择http
     ,input_charset:'utf-8'//字符编码格式 目前支持 gbk 或 utf-8
 };
